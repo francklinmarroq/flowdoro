8ab3dd16031cb7eb88cc6ad72357e53605a7f6e3
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-11-08 17:30:54 -0600

    fix: remove unused TaskDetails component from HomeView
    - Remove TaskDetails import and usage causing type errors
    - TaskDetails modal is already integrated in KanbanColumn component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="flex justify-center">
-        <TaskDetails />
         <div
             class="bg-white w-full xl:w-3/5 lg:w-4/5 p-8 mt-6 shadow-md rounded-xl"
         >
@@ -11,5 +10,4 @@
 
 <script setup lang="ts">
 import PomodoroContainer from "@/components/PomodoroContainer.vue";
-import TaskDetails from "@/components/TaskDetails.vue";
 </script>
