aa1ba34e11750fd90aad8e4f77b41e754189da2f
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-12-14 14:16:10 -0600

    refactor: componetize the navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import NavButton from "@/components/buttons/NavButton.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-    <div class="wrapper py-4 px-12 bg-background">
-        <div class="flex justify-center">
-            <nav
-                class="flex justify-around w-3/5 h-14 px-16 items-center shadow-xs rounded-xl bg-white"
-            >
-                <div class="flex grow">
-                    <p
-                        class="momo-trust-display-regular text-2xl font-bold antialiased text-primary-red"
-                    >
-                        flowdoro
-                    </p>
-                </div>
-                <div class="flex gap-3">
-                    <NavButton to="/">Home</NavButton>
-                    <!-- <NavButton to="/about">About</NavButton> -->
-                </div>
-            </nav>
-        </div>
-        <RouterView />
+  <div class="wrapper py-4 px-12 bg-background">
+    <div class="flex justify-center">
+      <Navbar />
     </div>
+    <RouterView />
+  </div>
 </template>
 
 <style></style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavButton from '@/components/buttons/NavButton.vue'
+</script>
+
+<template>
+  <nav class="flex justify-around w-3/5 h-14 px-16 items-center shadow-xs rounded-xl bg-white">
+    <div class="flex grow">
+      <p class="momo-trust-display-regular text-2xl font-bold antialiased text-primary-red">
+        flowdoro
+      </p>
+    </div>
+    <div class="flex gap-3">
+      <NavButton to="/">Home</NavButton>
+      <!-- <NavButton to="/about">About</NavButton> -->
+    </div>
+  </nav>
+</template>
